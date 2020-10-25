1562bacac50df171bd2e460c674e5a2b7d6f6568
tata1611 <ooorubiconch@gmail.com>
2020-10-26 00:40:47 +0300

    added new items

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,16 @@ $(function(){
          arrows: false,
          slidesToShow: 4,
          slidesToScroll: 4,
-
-
      });
+
+       $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
     
      var mixer = mixitup('.products__inner-box');
 });
